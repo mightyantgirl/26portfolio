b8e16b2d3f04b2ea463637db42d648aac1e74217
mightyantgirl <155346084+mightyantgirl@users.noreply.github.com>
2026-07-11 18:36:27 +0900

    💄 AboutMe 컴포넌트 스타일 수정: 레이아웃 및 여백 조정

--- src/page/aboutMe.jsx
+++ src/page/aboutMe.jsx
@@ -1,6 +1,6 @@
 export default function AboutMe({ onClose }) {
   return (
-    <div className="relative bg-[#f6ed5c] h-screen font-normal rounded-t-xl md:rounded-t-3xl px-5 py-15 md:px-20 md:pt-55 md:pb-20">
+    <div className="relative bg-[#f6ed5c] h-screen font-normal rounded-t-xl md:rounded-t-3xl px-5 py-15 md:px-20 md:pt-35 md:pb-20">
       {/* sm */}
       <div className="no-hidden md:hidden absolute flex justify-center items-center top-[-30px] left-0 w-1/3 bg-[#f6ed5c] rounded-t-2xl">
         <span className="no-hidden md:hidden text-lg font-semibold py-2 ">
@@ -8,7 +8,7 @@ export default function AboutMe({ onClose }) {
         </span>
       </div>
 
-      <div className="no-hidden md:hidden space-y-14 overflow-y-scroll h-full pt-3 pb-30">
+      <div className="no-hidden md:hidden space-y-14 overflow-y-scroll h-full pb-30">
         <div className="relative grid grid-cols-4 gap-4 items-start">
           <div className="flex flex-col gap-6 text-black">
             <div className="flex gap-2 font-semibold">
@@ -120,19 +120,19 @@ export default function AboutMe({ onClose }) {
       </div>
 
       {/* content */}
-      <div className="hidden md:block ">
-        <div className=" grid grid-cols-12 gap-6 text-lg">
-          <div className="flex flex-col space-y-3 text-black col-span-4">
+      <div className="hidden md:block overflow-y-scroll h-full pb-30">
+        <div className="grid grid-cols-12 gap-6 text-base">
+          <div className="flex flex-col space-y-2 text-black col-span-4">
             <p>P. 010.7137.0188</p>
             <p>M. jeenie0819@gmail.com</p>
             <p>github.com/mightyantgirl</p>
           </div>
 
-          <div className="col-span-8 flex flex-col gap-24">
+          <div className="col-span-8 flex flex-col gap-20">
             <div className="flex flex-col gap-6 text-black">
               <div className="flex gap-2 font-semibold">
-                <p className="text-xl">학력</p>
                 <p className="text-xs">01</p>
+                <p className="text-xl">학력</p>
               </div>
 
               <div className="flex flex-col gap-2">
@@ -157,31 +157,39 @@ export default function AboutMe({ onClose }) {
 
             <div className="flex flex-col gap-6 text-black">
               <div className="flex gap-2 font-semibold">
-                <p className="text-xl">경력</p>
                 <p className="text-xs">02</p>
+                <p className="text-xl">경력</p>
               </div>
-              <div className="flex flex-col gap-2">
+              <div className="flex flex-col gap-7">
                 <div className="grid grid-cols-[200px_1fr]">
-                  <p className="text-lg">2024. 09 - 2026. 02</p>
-                  <div>
-                    <span className="text-lg mr-2">인크루트 디자인팀</span>
-                    <span className="text-xs">웹 디자인/퍼블리싱</span>
-                  </div>
+                  <span className="text-lg">인크루트 디자인팀</span>
+                  <span className="text-lg">웹 디자인/퍼블리싱</span>
+                  <p className="text-xs mt-2">2024. 09 - 2026. 02</p>
+                  <p className="text-xs mt-2">
+                    인터넷 언론사 뉴스 기사 내에 설치하여 뉴스를 읽거나 출석을
+                    통해 얻은 보상을 코인으로 전환하고 출금할 수 있는 Widget과
+                    언론사가 코인의 스왑/출금/트랜잭션 내역을 볼 수 있는
+                    Explorer를 개발하였습니다.
+                  </p>
                 </div>
                 <div className="grid grid-cols-[200px_1fr]">
-                  <p className="text-lg">2020. 10 - 2021. 05</p>
-                  <div>
-                    <span className="text-lg mr-2">헤이컬렉티브 디자인팀</span>
-                    <span className="text-xs">UI/UX디자인</span>
-                  </div>
+                  <span className="text-lg ">헤이컬렉티브 디자인팀</span>
+                  <span className="text-lg">UI/UX디자인</span>
+                  <p className="text-xs mt-2">2020. 10 - 2021. 05</p>
+                  <p className="text-xs  mt-2">
+                    인터넷 언론사 뉴스 기사 내에 설치하여 뉴스를 읽거나 출석을
+                    통해 얻은 보상을 코인으로 전환하고 출금할 수 있는 Widget과
+                    언론사가 코인의 스왑/출금/트랜잭션 내역을 볼 수 있는
+                    Explorer를 개발하였습니다.
+                  </p>
                 </div>
               </div>
             </div>
 
             <div className="flex flex-col gap-6">
               <div className="flex gap-2 font-semibold text-black">
-                <p className="text-xl">기술</p>
                 <p className="text-xs">03</p>
+                <p className="text-xl">기술</p>
               </div>
               <div className="flex flex-col gap-7 text-lg text-black">
                 <div className="grid grid-cols-[200px_1fr]">
